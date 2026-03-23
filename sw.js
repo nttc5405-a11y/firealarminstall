cb8e4ff271ef7138184fc66debda5b7973041bc5
nttc5405-a11y <nttc5405@gmail.com>
2026-03-23 20:58:50 +0800

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'fire-app-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json'
+];
+
+// 安裝時快取檔案
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// 攔截請求，優先使用快取
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
